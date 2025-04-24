4b5c3580480f9316592ab6857886b1ebb6d18494
LorAccount <acctng.pa2@gmail.com>
2025-04-25 02:43:44 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '78e7ff90782e19e19c2c811618bc12e8';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
